f8d5527b582a19f78b79a9dc844598886638e4c3
snaylor86 <naylor86@gmail.com>
2017-04-08 14:15:44 -0600

    complete up through route activation

---
+++ app/events/event-thumbnail.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from  '@angular/core'
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+        <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
+            <h2>{{event.name}}</h2>
+            <div>Date: {{event.date}}</div>
+            <div>Time: {{event.time}}</div>
+            <div>Price: \${{event.price}}</div>
+            <div>
+                <span>Location: {{event.location.address}}</span>
+                <span class="pad-left"></span>
+                <span>
+                {{event.location.city}}, {{event.location.country}}
+                </span>
+            </div>
+        </div>
+    `,
+    styles: [`
+    .thumbnail { min-height: 210px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb;}
+    `]
+})
+
+export class EventThumbnailComponent {
+    @Input() event:any
+}
