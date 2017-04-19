79399cf66c9f39a841026e9cfeb8db3d318d29cc
snaylor86 <naylor86@gmail.com>
2017-04-18 20:44:15 -0600

    Finished up through Routing and Navigation

--- app/routes.ts
+++ app/routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router'
-import { EventsListComponent } from './events/events-list.component'
-import { EventDetailsComponent } from
-    './events/event-details/event-details.component'
-import { CreateEventComponent } from
-    './events/create-event.component'
+import {
+    EventsListComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator
+} from './events/index'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from 
-    './events/event-details/event-route-activator.service'
+
 
 export const appRoutes:Routes = [
     { path: 'events/new', component: CreateEventComponent,
@@ -15,5 +15,6 @@ export const appRoutes:Routes = [
     { path: 'events/:id', component: EventDetailsComponent,
         canActivate: [EventRouteActivator]},
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path: 'user', loadChildren: 'app/user/user.module#UserModule' }
 ]
